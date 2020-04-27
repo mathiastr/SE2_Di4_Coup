1af3b8c1578d4e876b0ee63d2d1905d163ad20f3
Amar Karabegovic <amarka@edu.aau.at>
2020-04-27 20:59:53 +0200

    Erstelltes Hauptmenü und Lobby-Bildschirm. Die Funktionalität für das Hauptmenü wurde hinzugefügt, so dass es auf den Lobby-Bildschirm wechselt.

--- app/src/main/java/com/example/coup/MainActivity.java
+++ app/src/main/java/com/example/coup/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.coup;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,9 +13,33 @@ public class MainActivity extends AppCompatActivity {
     // new Game
     //und jedem Spieler  bescheid sagen seine Ansicht anzeigen
 
+
+    //Amar
+    //Dies ist das Hauptmenü und führt zu weiteren Aktivitäten.
+    // Wir werden einen separaten Bildschirm für die Lobby und den Spielbildschirm haben.
+    // Dies ist die erste Aktivität, die gestartet wird.
+
+    private Button ButtonToLobby;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButtonToLobby = (Button) findViewById(R.id.button_play);
+
+        ButtonToLobby.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openLobby();
+            }
+        });
+
+
+    }
+
+    public void openLobby() {
+        Intent launchLobby = new Intent(this, LobbyScreen.class);
+        startActivity(launchLobby);
     }
 }
