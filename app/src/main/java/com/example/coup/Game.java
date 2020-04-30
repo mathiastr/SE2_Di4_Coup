406358e130c1a7cfeac12d8badf4a46acf8e1c26
Mathias Tragut <mathiastragut@gmx.at>
2020-04-30 13:55:36 +0200

    Game class

--- app/src/main/java/com/example/coup/Game.java
+++ app/src/main/java/com/example/coup/Game.java
@@ -1,10 +1,11 @@
 package com.example.coup;
 
+import com.example.coup.Action.Action;
+
 import java.util.*;
 
 public class Game {
     List<Player> players;
-    List<Card> playingCards;
     LayoutManager manager = new LayoutManager();
     Stack<Card> cards;
 
@@ -19,18 +20,42 @@ public class Game {
             cards.push(new Card(CardType.AMBASSADOR));
             cards.push(new Card(CardType.ASSASSIN));
         }
+
+
+        //for each Player set 2 random Cards and remove these cards from cards (deck)
         shuffleCards();
-        //for each Player set 2 random Cards and remove these cards from playingCards (deck)
+        for (int i  = 0; i < cards.size(); i++){
+
+            players.get(i).setCards(cards.subList(0,1));
+            cards.remove(0);
+            cards.remove(1);
+        }
+
         play();
     }
 
     private void play(){
-        //TODO
+
+
         //while 2 Players inGame = true
-            //get next player whos turn it is
-            //if player has more than 10 coins, Action Coup is required
-            //choosenAction = manager.next(players.get(x));
-            //choosenAction.playAction();
+        //get next player whos turn it is
+        while (twoinGame()){
+            for (int i  = 0; i < players.size(); i++){
+
+                //Action choosenAction = manager.next(players.get(i));
+                //choosenAction.playAction();
+
+            }
+
+        }
+        for (int i  = 0; i < players.size(); i++){
+            boolean inGame = players.get(i).getInGame();
+            if (inGame= true){
+                //  players.get(i) ist Gewinner
+            }
+
+        }
+
     }
     public void shuffleCards(){
         Collections.shuffle(cards);
@@ -43,5 +68,24 @@ public class Game {
     }
     public List<Card> getCards(){
         return this.cards;
+    }
+
+    //check if >1 Players are inGame
+    public boolean twoinGame(){
+
+        int inGame = 0;
+
+        for (int i  = 0; i < players.size(); i++) {
+            boolean x = players.get(i).getInGame();
+            if (x = true) {
+                inGame++;
+            }
+        }
+
+        if (inGame > 1) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
--- app/src/main/java/com/example/coup/LayoutManager.java
+++ app/src/main/java/com/example/coup/LayoutManager.java
@@ -1,10 +1,24 @@
 package com.example.coup;
 
+import com.example.coup.Action.Action;
+import com.example.coup.Action.Coup;
+
 public class LayoutManager {
 
-//    public Action next(Player currentTurn){
-//        //soll die Oberflächen der Spieler entsprechend ändern
-//        //und die Aktion der Spieler einlesen und an Game zurückgeben
-//        return null;
-//    }
+   /* public Action next(Player currentPlayer) {
+
+        if (currentPlayer.getCoins() >= 10) {
+            // if player has more than 10 coins, Action Coup is required (activate button Coup)
+        } else {
+
+            // activate buttons for other actions
+        }
+
+        // read choosen action
+        //Action choosenAction = new
+
+
+
+        return choosenAction;
+    }*/
 }
