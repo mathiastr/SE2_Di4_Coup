08dbbae26c7ded2c65d42b83fef3063641276a82
Robert Vukoja <vukojarob@hotmail.com>
2020-05-11 22:59:02 +0200

    added 4-player support

---
+++ Server/src/main/java/PlayerThreadOld.java
@@ -0,0 +1,97 @@
+public class PlayerThreadOld {
+
+    /**
+
+     //get Sockets
+
+     Socket player1 = players.get(0);
+     Socket player2 = players.get(1);
+
+     try {
+
+     //set up readers and writers
+
+     this.writer1 = new PrintWriter(new OutputStreamWriter(player1.getOutputStream()), true);
+     this.writer2 = new PrintWriter(new OutputStreamWriter(player2.getOutputStream()), true);
+     this.reader1 = new BufferedReader(new InputStreamReader(player1.getInputStream()));
+     this.reader2 = new BufferedReader(new InputStreamReader(player2.getInputStream()));
+
+     //decide turn
+     writer1.println("turn");
+     writer2.println("wait");
+
+     boolean check = true;
+
+     //game loop
+     while(check&&(isRunning(player1,player2))){
+     while(isRunning(player1,player2)){
+     String msg = reader1.readLine();
+     if(msg==null)
+     break;
+     else if(msg.equals("next")){
+     writer2.println("turn");
+     break;
+     }else
+     System.out.println(msg);
+     if(msg.equals("exit")){
+     writer2.println("win");
+     writer1.println("lose");
+     check=false;
+     break;
+     }
+     }
+
+     while (isRunning(player1,player2)){
+     String msg = reader2.readLine();
+     if(msg==null)
+     break;
+     else if(msg.equals("next")){
+     writer1.println("turn");
+     break;
+     }else
+     System.out.println(msg);
+     if(msg.equals("exit")){
+     writer1.println("win");
+     writer2.println("lose");
+     check=false;
+     break;
+     }
+
+     }
+     }
+
+
+     System.out.println("Connection to "+player1.getInetAddress().getHostAddress()+" closed");
+     System.out.println("Connection to "+player2.getInetAddress().getHostAddress()+" closed");
+     } catch (IOException e) {
+     e.printStackTrace();
+     //work in progress / the remaining player wins
+     if(player1.isConnected()){
+     try {
+     this.writer1 =new PrintWriter(new OutputStreamWriter(player1.getOutputStream()), true);
+     writer1.println("win");
+     } catch (IOException ex) {
+     ex.printStackTrace();
+     }
+     }else if(player2.isConnected()){
+     try {
+     this.writer2 =new PrintWriter(new OutputStreamWriter(player2.getOutputStream()), true);
+     writer2.println("win");
+     } catch (IOException ex) {
+     ex.printStackTrace();
+     }
+     }
+     }
+
+     }
+
+     private boolean isRunning(Socket socket1, Socket socket2){return socket1.isConnected()&&socket2.isConnected();}
+
+     private boolean allPlayersConnected(List<Socket> players){
+     for(Socket socket:players){
+     if(!socket.isConnected())
+     return false;}
+
+     return true;
+     }*/
+}
