1cc579e82bd9d910a1f4c61d4ac99aedea99cdd3
Amar Karabegovic <amarka@edu.aau.at>
2020-06-08 17:40:25 +0200

    Added UI Design to InGame activity and changed fonts.

--- app/src/main/java/com/example/coup/InGame.java
+++ app/src/main/java/com/example/coup/InGame.java
@@ -119,7 +119,7 @@ public class InGame extends Activity implements SensorEventListener {
 
 
         ConnectTask connectTask = new ConnectTask();
-        connectTask.execute();
+        // connectTask.execute();
 
         next.setOnClickListener(new View.OnClickListener() {
             @Override
