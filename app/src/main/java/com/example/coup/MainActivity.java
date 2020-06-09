3634f82894a4b1d36ec2729314b987b9c6bec203
Amar Karabegovic <amarka@edu.aau.at>
2020-06-10 01:17:09 +0200

    Implemented Main Menu design and created new button "About".

--- app/src/main/java/com/example/coup/MainActivity.java
+++ app/src/main/java/com/example/coup/MainActivity.java
@@ -7,7 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
+import android.widget.ImageButton;
 
 
 public class MainActivity extends Activity {
