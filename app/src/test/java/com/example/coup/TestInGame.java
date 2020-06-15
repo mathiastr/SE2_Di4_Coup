80a826fd60c8c94e9bf896f5f7abf7b06508f3a4
mesai <mesaislamovic@hotmail.com>
2020-06-15 21:30:26 +0200

    Added tests for Assassinate in inGame

--- app/src/test/java/com/example/coup/TestInGame.java
+++ app/src/test/java/com/example/coup/TestInGame.java
@@ -242,6 +242,124 @@ public class TestInGame {
 
 
     }
+    @Test
+    public void testEnemyAssassinated(){
+        List<String> enemies = new LinkedList<>();
+        enemies.add("player2");
+        enemies.add("player3");
+        enemies.add("player4");
+
+        inGame.initializeOpponents(enemies);
+
+        inGame.name="player1";
+
+        Player p = new Player();
+        p.setName("player2");
+
+        inGame.attackedPlayer=p;
+
+        String msg = "assassinate player1 player2";
+        String[] split = msg.split(" ");
+
+        inGame.handleMessage(msg,  split);
+
+        Assert.assertEquals("You used assassinate on player2", inGame.textView.getText().toString());
+
+
+    }
+
+    @Test
+    public void testAssassinatedByEnemy(){
+
+        List<String> enemies = new LinkedList<>();
+        enemies.add("player2");
+        enemies.add("player3");
+        enemies.add("player4");
+
+        inGame.initializeOpponents(enemies);
+
+        inGame.name="player1";
+
+        List<Card> cards =new LinkedList<>();
+        cards.add(new Card(CardType.AMBASSADOR));
+        cards.add(new Card(CardType.AMBASSADOR));
+
+        Player p1 = new Player();
+        p1.setName("player1");
+        p1.setCards(cards);
+
+
+        inGame.player=p1;
+
+        Player p2 = new Player();
+        p2.setName("player2");
+
+        inGame.attackedPlayer=p1;
+
+        String msg = "assassinate player2 player1";
+        String[] split = msg.split(" ");
+
+        inGame.handleMessage(msg,  split);
+
+        Assert.assertEquals("Assassinated: Choose a card to lose", inGame.textView.getText().toString());
+
+
+
+    }
+
+
+    @Test
+    public void testOtherPlayerAssassinated(){
+        List<String> enemies = new LinkedList<>();
+        enemies.add("player2");
+        enemies.add("player3");
+        enemies.add("player4");
+
+        inGame.initializeOpponents(enemies);
+
+        inGame.name="player1";
+
+        Player p = new Player();
+        p.setName("player2");
+
+        inGame.attackedPlayer=p;
+
+        String msg = "assassinate player3 player2";
+        String[] split = msg.split(" ");
+
+        inGame.handleMessage(msg,  split);
+
+        Assert.assertEquals("player3 used assassinate on player2", inGame.textView.getText().toString());
+
+
+    }
+
+
+    @Test
+    public void testAsssassinateNoCoins(){
+        inGame.assassinateButton.performClick();
+
+        Assert.assertEquals("You need 3 coins to use assassinate", inGame.textView.getText().toString());
+    }
+
+    @Test
+    public void testAssassinate(){
+
+        inGame.player.setCoins(3);
+
+        Player attacked = new Player();
+        attacked.setName("player2");
+
+        inGame.attackedPlayer=attacked;
+
+        inGame.assassinateButton.performClick();
+
+        inGame.doAction("assassinate");
+
+        Assert.assertEquals("Assassinating "+inGame.attackedPlayer.getName(), inGame.textView.getText().toString());
+
+    }
+
 
 
 
